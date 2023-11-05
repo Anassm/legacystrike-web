84c11ca547ca0e3192de1b96b217c07d87e0ea95
Anass <a.moussadi03@gmail.com>
2023-11-05 20:40:33 +0100

    Stats page WIP

--- src/components/Pages/Stats/Stats.jsx
+++ src/components/Pages/Stats/Stats.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
+import mirage from "../../../assets/static/maps/mirage.jpg";
 
 export default function Stats() {
   const [matchData, setMatchData] = useState([]);
 
+  const apiPort = import.meta.env.VITE_API_PORT;
+
   async function getDemos() {
     try {
-      const res = await fetch("http://localhost:3000/api/recent-matches");
+      const res = await fetch(`http://localhost:${apiPort}/api/recent-matches`);
       const data = await res.json();
       setMatchData(data);
     } catch (err) {
@@ -20,15 +23,37 @@ export default function Stats() {
 
   return (
     <>
-      <h1>STATS PAGE</h1>
-      {matchData.map((match, index) => (
-        <div key={index}>
-          <span>{match.matchData.ctScore}</span>
-          <span>{match.matchData.tScore}</span>
-          <span>{match.players[0].name}</span>
-          <span>{match.players[0].steamId}</span>
+      <div className="flex flex-col items-center mt-20">
+        <h1 className="mb-6 text-lg font-bold">
+          Recent {matchData.length} games
+        </h1>
+
+        <div className="w-60">
+          <div className="flex justify-between mb-4">
+            <span className="font-bold">CT</span>
+            <span className="font-bold">T</span>
+          </div>
+          {matchData.map((match, index) => (
+            <div
+              key={index}
+              className={`${
+                match.matchData.ctScore > match.matchData.tScore
+                  ? "bg-ct-blue"
+                  : ""
+              } ${
+                match.matchData.tScore > match.matchData.ctScore
+                  ? "bg-t-orange"
+                  : ""
+              }
+                mb-2 pl-6 pr-6 pt-4 pb-4 rounded-md`}
+            >
+              <span className="flex justify-center font-bold text-white text-center">{`${match.matchData.ctScore} - ${match.matchData.tScore}`}</span>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </>
   );
 }
+
+// `bg-mirage bg-cover bg-center`
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ["./index.html", "./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {
+      backgroundImage: {
+        mirage: "url('./src/assets/static/maps/mirage.jpg')",
+      },
+      backgroundSize: {
+        auto: "auto",
+        cover: "cover",
+        contain: "contain",
+        fill: "fill",
+      },
+      backgroundPosition: {
+        center: "center center",
+      },
       colors: {
         "discord-blue": "#738ADB",
         "ct-blue": "#3b415c",
         "t-orange": "#e48717",
       },
+    },
+    letterSpacing: {
+      score: "-1.05em",
     },
   },
   plugins: [],
